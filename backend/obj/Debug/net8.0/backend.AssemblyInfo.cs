86dc49e2e8db9f79ad36cdbe20b122020d611a82
brunotrindadefm <btrindade172@gmail.com>
2024-09-18 14:49:25 -0300

    feat: add method post and form angular

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ee1b49514512af11f6138b4b5d5ea578d625807")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3806e567a6df6f13ce4df913fe598a6108aaa7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/app/components/form/form.component.ts
+++ frontend/src/app/components/form/form.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule  } from '@angular/forms';
+import { Student } from '../../model/Student';
+import { StudentsService } from '../../services/students.service';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
 export class FormComponent {
+
+  form: any;
+
+  constructor(private studentsService: StudentsService) { }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      name: new FormControl(null),
+      lastName: new FormControl(null),
+      dateOfBirth: new FormControl(null),
+      grade1: new FormControl(null),
+      grade2: new FormControl(null),
+      institutionalGrade: new FormControl(null),
+      activities: new FormControl(null),
+    })
+  }
+
+  sendForm(): void {
+    if (this.form.value > 0) {
+      const student: Student = this.form.value;
+      this.studentsService.createStudent(student).subscribe(result => {
+        alert('Aluno inserido com sucesso');
+        this.form.reset(); 
+      });
+    } else {
+      alert('Por favor, preencha todos os campos corretamente.');
+    }
+  }
 
 }
